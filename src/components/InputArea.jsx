6fded4156c83ba9af62ae0f64bf542b39d0f65fe
CyberPraetor <34304882+jooufree@users.noreply.github.com>
2025-03-01 06:42:29 +0400

    Revert "Финальная версия"

--- src/components/InputArea.jsx
+++ src/components/InputArea.jsx
@@ -1,20 +1,6 @@
-import { createUserTask } from '../api/http';
-import { useState } from 'react';
 import classes from './InputArea.module.css';
 
-export default function InputArea({ updateTasks, taskFilter }) {
-  const [inputValue, setInputValue] = useState('');
-
-  async function handleAddTask(value) {
-    if (value.length > 1 && value.length < 65) {
-      await createUserTask(value);
-      setInputValue('');
-      await updateTasks(taskFilter);
-    } else if (value.length < 2) {
-      alert('Задача должна состоять минимум из 2 символов!');
-    }
-  }
-
+export default function InputArea({ handleChange, inputValue, setInputValue }) {
   return (
     <section className={classes['input-area']}>
       <input
@@ -28,7 +14,7 @@ export default function InputArea({ updateTasks, taskFilter }) {
       />
       <button
         className={classes.button}
-        onClick={async () => await handleAddTask(inputValue)}
+        onClick={() => handleChange(inputValue)}
       >
         Add
       </button>
